37f07ed70a45d2528ae982032edf746c4b78ebb8
Lindsey Miller <lindc.miller@gmail.com>
2015-10-09 09:30:27 -0400

    fix ruby version, pass rspec valid item tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,50 +7,11 @@ RSpec.describe Item, type: :model  do
   it { is_expected.to respond_to(:shelf_life_days) }
   it { is_expected.to respond_to(:stock_code) }
 
-  # describe "complete item" do
-  #   subject(:item) { build(:item) }
-  #   it "has a name, price, quantity, shelf life, stock code" do
-  #     expect(item).to be_valid
-  #     expect(item.name).to be_a(String)
-  #     expect(item.price).to be_a(Integer)
-  #     expect(item.quantity).to be_a(Integer)
-  #     expect(item.shelf_life_days).to be_a(Integer)
-  #     expect(item.stock_code).to be_a(String)
-  #   end
-  # end
-
-  # describe "#search" do
-  #   let(:item) { create(:item) }
-  #   it "finds an item by search form" do
-  #     item
-  #     items = Item.all
-  #     found = items.search("Eggs")
-  #     expect(found.any? {|h| h[:name] == "Eggs"}).to be_truthy
-  #   end
-  #     it "searches for an item and doesn't find it" do
-  #     item
-  #     items = Item.all
-  #     found = items.search("Shoes")
-  #     expect(found.any? {|h| h[:name] == "Shoes"}).to be_falsey
-  #   end
-  # end
-
-  # describe "#not_expired" do
-  #   let(:item) { create(:item) }
-  #   it "lists unexpired items" do
-  #     item
-  #     items = Item.all
-  #     found = items.not_expired
-  #     expect(found.count).to eq(1)
-  #   end
-  #   it "does not include expired items" do
-  #     item
-  #     item.shelf_life_days = -1
-  #     item.save
-  #     items = Item.all
-  #     found = items.not_expired
-  #     expect(found.count).to eq(0)
-  #   end
-  # end
+  describe Item do
+    it "has a name, price, quantity, shelf life, stock code" do
+      item = Item.new(name: "Eggs", price: 4, quantity: 12, shelf_life_days: 7, stock_code: "3GG2")
+      expect(item).to be_valid
+    end
+  end
 
 end
